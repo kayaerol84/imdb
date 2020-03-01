682e0cd63d98a8145fb2b8d166ad96d5b922ba92
Erol Shaban <kayaerol84@gmail.com>
2020-03-01 16:56:52 +0100

    init

---
+++ src/test/java/com/yucel/bootcamp/imdb/ImdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yucel.bootcamp.imdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
